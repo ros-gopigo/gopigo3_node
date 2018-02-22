226bb88638c16dbab6c0710fb574556ceb681ef1
Christian Rauch <Rauch.Christian@gmx.de>
2018-02-22 01:01:45 +0000

    publish encoder readings in degree

--- src/gopigo3_node.py
+++ src/gopigo3_node.py
@@ -11,31 +11,41 @@ except IOError as e:
     sys.exit()
 
 import rospy
-from std_msgs.msg import Int8
+from std_msgs.msg import Int8, Float64
 
 
 class Robot:
-    def __init__(self):
-        # GoPiGo3 setup
-        self.g = gopigo3.GoPiGo3()
+    # short variables
+    ML = gopigo3.GoPiGo3.MOTOR_LEFT
+    MR = gopigo3.GoPiGo3.MOTOR_RIGHT
 
-        # ROS setup
+    def __init__(self):
+        # GoPiGo3 and ROS setup
+        self.g = gopigo3.GoPiGo3()
         rospy.init_node("gopigo3")
 
-        # low-level access
+        # subscriber
         rospy.Subscriber("motor/pwm/left", Int8, self.on_motor_pwm_left)
         rospy.Subscriber("motor/pwm/right", Int8, self.on_motor_pwm_right)
 
-    def start(self):
-        rospy.spin()
+        # publisher
+        self.pub_enc_l = rospy.Publisher('motor/encoder/left', Float64, queue_size=10)
+        self.pub_enc_r = rospy.Publisher('motor/encoder/right', Float64, queue_size=10)
+
+        # main loop
+        rate = rospy.Rate(10)   # in Hz
+        while not rospy.is_shutdown():
+            self.pub_enc_l.publish(Float64(data=self.g.get_motor_encoder(self.ML)))
+            self.pub_enc_r.publish(Float64(data=self.g.get_motor_encoder(self.MR)))
+
+            rate.sleep()
 
     def on_motor_pwm_left(self, msg):
-        self.g.set_motor_power(self.g.MOTOR_LEFT, msg.data)
+        self.g.set_motor_power(self.ML, msg.data)
 
     def on_motor_pwm_right(self, msg):
-        self.g.set_motor_power(self.g.MOTOR_RIGHT, msg.data)
+        self.g.set_motor_power(self.MR, msg.data)
 
 
 if __name__ == '__main__':
     r = Robot()
-    r.start()
