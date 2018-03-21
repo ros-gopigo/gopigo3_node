035e1eee95858435d65d59fff14b52bb33a470bd
Quinty van Dijk <quintvdijk@gmail.com>
2018-03-21 15:18:04 +0100

    added time stamp to tf message

--- src/gopigo3_driver.py
+++ src/gopigo3_driver.py
@@ -257,7 +257,7 @@ class Robot:
         odom = Odometry(header=Header(frame_id="world"), child_frame_id="gopigo",
                         pose=self.pose, twist=twist)
 
-        transform = TransformStamped(header=Header(frame_id="world"), child_frame_id="gopigo")
+        transform = TransformStamped(header=Header(stamp=rospy.Time.now(), frame_id="world"), child_frame_id="gopigo")
         transform.transform.translation.x = self.pose.pose.position.x
         transform.transform.translation.y = self.pose.pose.position.y
         transform.transform.translation.z = self.pose.pose.position.z
